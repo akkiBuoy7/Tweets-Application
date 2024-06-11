aa70f7c19209078fe77292d364c4bef3318dce2d
akkiB <akash3350saha@gmail.com>
2024-06-12 01:29:23 +0530

    initial commit

---
+++ app/src/main/java/com/example/tweetsapplication/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.tweetsapplication
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.example.tweetsapplication.ui.theme.TweetsApplicationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TweetsApplicationTheme {
+                Text(text = "AKASH", fontSize = 50.sp)
+
+            }
+        }
+    }
+}
